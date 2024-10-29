fda181212605533821a7b18fc3b5c086ff7b16a6
staceyc <katiestacey44@gmail.com>
2024-10-29 08:38:15 -0400

    Initial Commit

---
+++ Demo_delete/Demo_deleteApp.swift
@@ -0,0 +1,17 @@
+//
+//  Demo_deleteApp.swift
+//  Demo_delete
+//
+//  Created by Catherine L. Stacey on 10/29/24.
+//
+
+import SwiftUI
+
+@main
+struct Demo_deleteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Demo_deleteUITests/Demo_deleteUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Demo_deleteUITestsLaunchTests.swift
+//  Demo_deleteUITests
+//
+//  Created by Catherine L. Stacey on 10/29/24.
+//
+
+import XCTest
+
+final class Demo_deleteUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
